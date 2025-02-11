d0d8cd5f7234bdff0c6010a645faeda64a683f25
StephenSulimani <stephensulimani@gmail.com>
2025-02-10 23:28:19 -0500

    :tada: added codeium!

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,19 +1,24 @@
 return {
     "danymat/neogen",
     config = function()
-        local neogen = require('neogen').setup({
+        local neogen = require("neogen").setup({
             enabled = true,
             languages = {
                 python = {
                     template = {
-                        annotation_convention = "reST"
-                    }
-                }
-            }
+                        annotation_convention = "reST",
+                    },
+                },
+                typescript = {
+                    template = {
+                        annotation_convention = "tsdoc",
+                    },
+                },
+            },
         })
-        vim.keymap.set('n', '<leader>gd', ":lua require('neogen').generate()<CR>", {
+        vim.keymap.set("n", "<leader>gd", ":lua require('neogen').generate()<CR>", {
             noremap = true,
-            silent = true
-        } )
-    end
+            silent = true,
+        })
+    end,
 }
