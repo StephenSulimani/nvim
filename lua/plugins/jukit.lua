2a8ee4ecc158c8c229233f72118c2c7c5ee2c596
StephenSulimani <stephensulimani@gmail.com>
2025-02-18 16:45:17 -0500

    :wrench: removed annoying <CR> jukit bind

--- lua/plugins/jukit.lua
+++ lua/plugins/jukit.lua
@@ -2,5 +2,21 @@ return {
     "luk400/vim-jukit",
     config = function()
         vim.g.jukit_convert_open_default = 0
+        local function safe_unmap(mode, lhs)
+            local mappings = vim.api.nvim_get_keymap(mode)
+            for _, mapping in ipairs(mappings) do
+                if mapping.lhs == lhs then
+                    vim.api.nvim_del_keymap(mode, lhs)
+                    return
+                end
+            end
+        end
+        safe_unmap("n", "<leader><CR>")
+        vim.api.nvim_set_keymap(
+            "n",
+            "<leader><CR>",
+            "<Cmd>call jukit#send#line()<CR>",
+            { noremap = true, silent = true }
+        )
     end,
 }
