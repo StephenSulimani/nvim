5ac8a3fe876e00d4766ba15bd5c4b779c10391fe
StephenSulimani <stephen.sulimani@gmail.com>
2025-03-14 02:48:48 -0400

    :tada: auto detects python3

--- init.lua
+++ init.lua
@@ -1,16 +1,30 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-vim.g.python3_host_prog = '/home/stephen/.local/share/mise/installs/python/3.13.2/bin/python3'
+-- vim.g.python3_host_prog = '/home/stephen/.local/share/mise/installs/python/3.13.2/bin/python3'
+
+local function find_python3()
+	local handle = io.popen("which python3") -- or "command -v python3" for more compatibility
+	local result = handle:read("*a")
+	handle:close()
+	return result:gsub("%s+", "") -- Trim whitespace
+end
+
+local python3_path = find_python3()
+if python3_path and python3_path ~= "" then
+	vim.g.python3_host_prog = python3_path
+else
+	print("Python 3 executable not found. Please set vim.g.python3_host_prog manually.")
+end
 
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
