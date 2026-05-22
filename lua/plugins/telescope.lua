2348d07624683106811410566513696f9ce9952e
Stephen Sulimani <stephensulimani@gmail.com>
2026-05-21 22:21:04 -0400

    fix: upgrade Telescope for nvim-treesitter main compatibility
    Pin telescope.nvim to 0.2.2 (0.1.x used removed ft_to_lang API) and
    consolidate ui-select setup. Disable preview treesitter as a fallback.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,26 +1,31 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local builtin = require("telescope.builtin")
-            
-            vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-        end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-            require("telescope").load_extension("ui-select")
-        end,
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		-- 0.1.x calls nvim-treesitter.parsers.ft_to_lang (removed on treesitter main)
+		version = "0.2.2",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
+
+			telescope.setup({
+				preview = {
+					-- Avoid ft_to_lang crash if an old telescope build is still on disk
+					treesitter = false,
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			telescope.load_extension("ui-select")
+
+			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+		end,
+	},
 }
