a6fd524f560e142f93133e67f322355673110e8c
Stephen Sulimani <stephensulimani@gmail.com>
2026-05-23 01:10:14 -0400

    fix: allow DiffviewClose when diff tab has modified buffers
    Temporarily set hidden while closing so tabclose does not raise E445
    when reviewing changes before pushing to GitHub.

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,11 +1,20 @@
 return {
 	"sindrets/diffview.nvim",
 	config = function()
+		local function diffview_close()
+			-- Diffview closes its tab with :tabclose, which raises E445 when another
+			-- window in that tab is modified (common when reviewing a PR diff).
+			local hidden = vim.o.hidden
+			vim.o.hidden = true
+			pcall(require("diffview").close)
+			vim.o.hidden = hidden
+		end
+
 		vim.keymap.set("n", "<leader>dc", function()
 			if next(require("diffview.lib").views) == nil then
 				vim.cmd("DiffviewOpen")
 			else
-				vim.cmd("DiffviewClose")
+				diffview_close()
 			end
 		end)
 	end,
